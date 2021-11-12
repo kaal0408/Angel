a0cc5a13ee0de1ac677b876cd09760d6e55e53a0
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-11-12 06:58:12 +0530

    Update utils.py

--- Angel/utils.py
+++ Angel/utils.py
@@ -4,11 +4,11 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"Raizen/plugins/{plugin_name}.py")
-    name = "Raizen.plugins.{}".format(plugin_name)
+    path = Path(f"Angel/plugins/{plugin_name}.py")
+    name = "Angel.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["Raizen.plugins." + plugin_name] = load
-    print("Raizen has Imported " + plugin_name)
+    sys.modules["Angel.plugins." + plugin_name] = load
+    print("Angel has Imported " + plugin_name)
