4122a504ba841ce4813f8c47dc05785e7e2a6d4f
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-11-12 07:13:30 +0530

    Update sed.py

--- Angel/plugins/sed.py
+++ Angel/plugins/sed.py
@@ -1,23 +1,23 @@
 from .. import Raizen
 from telethon import events, Button, client
 
-Raizen_USER = [1801571739]
+Angel_USER = [2068551800]
 
-@Raizen.on(
-    events.NewMessage(pattern="^/sed ?(.*)", func=lambda e: e.sender_id in Raizen_USER)
+@Angel.on(
+    events.NewMessage(pattern="^/sed ?(.*)", func=lambda e: e.sender_id in Angel_USER)
 )
 async def amdddd(event):
     text = event.pattern_match.group(1)
     k = [[Button.text(text)]]
-    await Raizen.send_message(event.chat_id, "ʀᴀɪᴢᴇɴ 💝", buttons=k)
+    await Angel.send_message(event.chat_id, "Angel 💝", buttons=k)
     
-@Raizen.on(events.NewMessage(pattern="^/sed"))
+@Angel.on(events.NewMessage(pattern="^/sed"))
 async def start_all(event):
     if event.is_private:
         await event.reply("ᴜsᴇ ᴛʜɪs ᴄᴍᴅ ɪɴ ɢʀᴏᴜᴘ")
 ###################################################
-@Raizen.on(events.callbackquery.CallbackQuery(data="creator"))
+@Angel.on(events.callbackquery.CallbackQuery(data="creator"))
 async def creator(event):
-    await event.edit(event.chat_id, "ʜᴇʀᴇ ɪs ᴍʏ ᴍᴀsᴛᴇʀ ᴜsᴇʀɴᴀᴍᴇ  @DIPESH_XD")
+    await event.edit(event.chat_id, "ʜᴇʀᴇ ɪs ᴍʏ ᴍᴀsᴛᴇʀ ᴜsᴇʀɴᴀᴍᴇ  @Hayat_Murat_30")
 
 #######################################################################################################################################
