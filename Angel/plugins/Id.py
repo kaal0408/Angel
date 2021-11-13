79cfda8b65f9c1d00e32ba61d3f4a45b4bafee4f
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-11-13 12:48:24 +0530

    Update Id.py

--- Angel/plugins/Id.py
+++ Angel/plugins/Id.py
@@ -1,29 +1,29 @@
-from .. import Raizen
+from .. import Angel
 from telethon import events
 from telethon.utils import pack_bot_file_id
 
 
-@Raizen.on(events.NewMessage(pattern="^/id"))
+@Angel.on(events.NewMessage(pattern="^/id"))
 async def _(event):
     if event.reply_to_msg_id:
         await event.get_input_chat()
         r_msg = await event.get_reply_message()
         if r_msg.media:
             bot_api_file_id = pack_bot_file_id(r_msg.media)
-            await Raizen.send_message(
+            await Angel.send_message(
                 event.chat_id,
                 "Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(
                     str(event.chat_id), str(r_msg.from_id), bot_api_file_id
                 ),
             )
         else:
-            await Raizen.send_message(
+            await Angel.send_message(
                 event.chat_id,
                 "Current Chat ID: `{}`\nFrom User ID: `{}`".format(
                     str(event.chat_id), str(r_msg.from_id)
                 ),
             )
     else:
-        await Raizen.send_message(
+        await Angel.send_message(
             event.chat_id, "Current Chat ID: `{}`".format(str(event.chat_id))
         )
